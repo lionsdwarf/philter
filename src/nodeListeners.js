be7b692b1790c0c5a0c8ca1fa90605be35083075
Joe Leinsdorf <leinsdorf@gmail.com>
2017-09-04 10:20:09 -0600

    node actions refactor > mappings

--- src/nodeListeners.js
+++ src/nodeListeners.js
@@ -1,48 +1,25 @@
 const electron = window.require('electron')
 const ipcRenderer = electron.ipcRenderer
 
+const actionMappings = {
+  'source-dir-selection': 'SET_SOURCE_DIR',
+  'target-dir-selection': 'ADD_TARGET_DIRS',
+  'source-dir-contents': 'SET_SOURCE_JPGS',
+  'drive-dirs': 'SET_DRIVE_DIRS',
+  'thumb-fileName': 'SET_THUMB_FILENAME',
+  'thumbs-source-dir': 'SET_THUMBS_SOURCE_DIR'
+}
+
 export const initListeners = dispatch => {
-  
-  ipcRenderer.on('source-dir-selection', (event, payload) => {
-    dispatch({
-      type: 'SET_SOURCE_DIR',
-      payload: payload
-    })
-  })
 
-  ipcRenderer.on('target-dir-selection', (event, payload) => {
-    dispatch({
-      type: 'ADD_TARGET_DIRS',
-      payload: payload
+  for (let nodeAction in actionMappings) {
+    ipcRenderer.on(nodeAction, (event, payload) => {
+      dispatch({
+        type: actionMappings[nodeAction],
+        payload: payload
+      })
     })
-  })
-
-  ipcRenderer.on('source-dir-contents', (event, payload) => {
-    dispatch({
-      type: 'SET_SOURCE_JPGS',
-      payload: payload
-    })
-  })
-
-  ipcRenderer.on('drive-dirs', (event, payload) => {
-    dispatch({
-      type: 'SET_DRIVE_DIRS',
-      payload: payload
-    })
-  })
-
-  ipcRenderer.on('thumb-fileName', (event, payload) => {
-    dispatch({
-      type: 'SET_THUMB_FILENAME',
-      payload: payload
-    })
-  })
-
-  ipcRenderer.on('thumbs-source-dir', (event, payload) => {
-    dispatch({
-      type: 'SET_THUMBS_SOURCE_DIR',
-      payload: payload
-    })
-  })
+  }  
 
 }
+
