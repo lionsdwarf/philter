556c48ca3d1d0ef0d9947d35bcb44c326f804316
Joe Leinsdorf <leinsdorf@gmail.com>
2017-10-10 22:30:25 -0400

    conditional raw file sync dependant on raw file existence, or jpg

--- node/diskSync.js
+++ node/diskSync.js
@@ -1,9 +1,23 @@
 const fs = require('fs')
 const path = require('path')
 
+const OLYMPUS_RAW_FILE_EXT = 'ORF'
+
+const swapRawFileExtension = jpgFileName => {
+  return jpgFileName.substr(0, jpgFileName.length - 3) + OLYMPUS_RAW_FILE_EXT
+}
+
+//if raw file exists, write it to disk, otherwise write jpg
+const getSourceFileName = (sourceDir, jpgFileName) => {
+  const rawFileName = swapRawFileExtension(jpgFileName)
+  const rawFile = path.join(sourceDir, rawFileName)
+  return fs.existsSync(rawFile) ? rawFileName : jpgFileName
+}
+
 const copyFileToDisk = (sourceDir, targetDir, fileName, eventEmitter) => {
-  const sourceFile = path.join(sourceDir, fileName)
-  const targetFile = path.join(targetDir, fileName)
+  const sourceFileName = getSourceFileName(sourceDir, fileName)
+  const sourceFile = path.join(sourceDir, sourceFileName)
+  const targetFile = path.join(targetDir, sourceFileName)
   const readStream = fs.createReadStream(sourceFile)
   const writeStream = fs.createWriteStream(targetFile)
   
@@ -17,7 +31,7 @@ const copyFileToDisk = (sourceDir, targetDir, fileName, eventEmitter) => {
   })
 
   writeStream.on('error', (err) => {
-    console.log('writeStream error: ', err)
+    console.log('Disk write stream error: ', err)
     eventEmitter.send('disk-write-error', {
       targetDir: targetDir,
       fileName: fileName,
@@ -26,7 +40,7 @@ const copyFileToDisk = (sourceDir, targetDir, fileName, eventEmitter) => {
   })
 
   readStream.on('error', (err) => {
-    console.log('readStream error: ', err)
+    console.log('Disk read stream error: ', err)
     eventEmitter.send('disk-write-error', {
       targetDir: targetDir,
       fileName: fileName,
