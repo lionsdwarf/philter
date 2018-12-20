4c508a1522d3532349d9d2a9fba580910f654a7e
Joe Leinsdorf <leinsdorf@gmail.com>
2018-12-20 09:46:14 -0700

    .thumbs write directories in build/ and public/

--- node/constants/thumbnails.js
+++ node/constants/thumbnails.js
@@ -1,4 +1,4 @@
-const thumbs = process.argv[3] === 'dev' ? 'public/' : 'build/'
+const thumbs = `${process.argv[3] === 'dev' ? 'public' : 'build'}/.thumbs/`
 // const thumbs = '.thumbnails/'
 
 module.exports = {
