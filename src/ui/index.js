c0db90639873d92b35e54d287adec3f63736eca1
Joe Leinsdorf <leinsdorf@gmail.com>
2018-12-28 11:01:27 -0700

    code cleanup, black Inspect background

--- src/ui/index.js
+++ src/ui/index.js
@@ -6,34 +6,11 @@ let src
 
 const setMainImg = (payload) => {
   src = `${path.join(payload.sourceDir, payload.fileName)}`
-  $('.tile')
-    .children('.photo').css({'background-image': `url(file://${src})`});
-  // const img = document.getElementById('main-img')
-  // img.src = src
-  // const img = document.createElement('img')
-  // img.src = `${path.join(payload.sourceDir, payload.fileName)}`
-  // rootEl.append(img)
+  $('#img-wrapper').children('#img').css({'background-image': `url(file://${src})`});
 }
 
 ipcRenderer.on('set-main-img', (e, payload) => setMainImg(payload))
 
-$('.tile')
-  // tile mouse actions
-  // .on('mouseover', function(){
-  //   $(this).children('.photo').css({'transform': 'scale(2)'});
-  // })
-  // .on('mouseout', function(){
-  //   $(this).children('.photo').css({'transform': 'scale(1)'});
-  // })
-  .on('mousemove', function(e){
-    $(this).children('.photo').css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +'%'});
-  })
-  // // // tiles set up
-  // .each(function(){
-  //   $(this)
-      // add a photo container
-      // .append('<div class="photo"></div>')
-      // some text just to show zoom level on current item in this example
-      // .children('.photo').css({'background-image': 'url('+ $(this).attr('data-image') +')'});
-      // .children('.photo').css({'background-image': `url(${src})`});
-  // })
+$('#img-wrapper').on('mousemove', function(e){
+  $(this).children('#img').css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +'%'});
+})
